73f29a07d7bf9231367eac16fac664d577622365
gyf1150554303 <1150554303@qq.com>
2021-01-09 15:27:32 +0800

    完成了商品列表页面的开发

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -1,6 +1,7 @@
 <template>
   <view>
     <view class="search-box">
+      <!-- 使用 uni-ui 提供的搜索组件 -->
       <uni-search-bar @input="input" :radius="100" cancelButton="none"></uni-search-bar>
     </view>
 
@@ -31,12 +32,15 @@
   export default {
     data() {
       return {
+        // 延时器的 timerId
         timer: null,
+        // 搜索关键词
         kw: '',
         // 搜索的结果列表
         searchResults: [],
         // 搜索历史的数组
-        historyList: []
+        historyList: [],
+        
       };
     },
     onLoad() {
@@ -45,13 +49,18 @@
     methods: {
       // input 输入事件的处理函数
       input(e) {
+        // 清除 timer 对应的延时器
         clearTimeout(this.timer)
 
+        // 重新启动一个延时器，并把 timerId 赋值给 this.timer
         this.timer = setTimeout(() => {
+          // 如果 500 毫秒内，没有触发新的输入事件，则为搜索关键词赋值
           this.kw = e.value
+          // 根据关键词，查询搜索建议列表
           this.getSearchList()
         }, 500)
       },
+      // 根据搜索关键词，搜索商品建议列表
       async getSearchList() {
         // 判断搜索关键词是否为空
         if (this.kw.length === 0) {
@@ -59,6 +68,7 @@
           return
         }
 
+        // 发起请求，获取搜索建议列表
         const {
           data: res
         } = await uni.$http.get('/api/public/v1/goods/qsearch', {
@@ -71,6 +81,7 @@
       },
       gotoDetail(item) {
         uni.navigateTo({
+          // 指定详情页面的 URL 地址，并传递 goods_id 参数
           url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
         })
       },
@@ -123,8 +134,11 @@
       border-bottom: 1px solid #efefef;
 
       .goods-name {
+        // 文字不允许换行（单行文本）
         white-space: nowrap;
+        // 溢出部分隐藏
         overflow: hidden;
+        // 文本溢出后，使用 ... 代替
         text-overflow: ellipsis;
         margin-right: 3px;
       }
